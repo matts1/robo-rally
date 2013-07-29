567a255c29f699b8f60ebe6cdf0593c9ffdfdaf5
matts1 <mattstark75@gmail.com>
2013-07-29 19:07:11 +1000

    Upgraded the filtering to be quicker (cached the whole table, rather than passing it each time) - still slow

--- robo_rally/courses/views.py
+++ robo_rally/courses/views.py
@@ -5,6 +5,6 @@ class PickMapView(TemplateView):
     template_name = 'courses/maplist.html'
     def get_context_data(self, **kwargs):
         maps=Course.objects.all().values(
-            'name', 'length', 'difficulty', 'min_players', 'max_players'
+            'name', 'length', 'difficulty', 'min_players', 'max_players', 'filename'
         ).order_by('name')
         return dict(maps=maps)
--- robo_rally/static/js/table.js
+++ robo_rally/static/js/table.js
@@ -1,71 +1,79 @@
-document.ready = function () {
-    tables = $(".magictable")
-    for (var i = 0; i < tables.length; i++) {
-        setupTable(tables[i]);
+function to2d (whole) {
+    rows = whole.children();
+    for (var y = 0; y < rows.length; y++) {
+        rows[y] = $(rows[y]).children();
+        for (var x = 0; x < rows[y].length; x++) {
+            rows[y][x] = $(rows[y][x]);
+        }
     }
+    return rows;
 }
 
-function setupTable (table) {
-    thead = $("thead", table);
+document.ready = function () {
+    if ($("table#magictable") == undefined) {
+        console.log("no magictable");
+        return;
+    }
+    window.thead = to2d($("table#magictable thead"));
+    window.tbody = to2d($("table#magictable tbody"));
     needFilterRow = false;
     filterRow = $("<tr></tr>");
-    row = $("tr th", thead);
-    for (var i = 0; i < row.length; i++) {
-        filter = $(row[i]).attr("data-filter");
+    cols = window.thead[0];
+    for (var i = 0; i < cols.length; i++) {
+        filter = $(cols[i]).attr("data-filter");
         col = $("<th></th>")
         if (filter != undefined) {
             needFilterRow = true;
         }
         if (filter == "text") {
             col.append("<input>");
-            col.on('keyup', 'input', textFilter);
+            col.on('keyup', 'input', {method: "startword", index: i}, textFilter);
         } else if (filter == "select") {
             col.append("<select><option value=''>All</option></select>");
-            rows = $("tbody tr", table);
-            vals = {};
-            for (var j = 0; j < rows.length; j++) {
-                vals[$($(rows[j]).children()[i]).text()] = true;
-            }
             select = $("select", col);
-            for (var key in vals) {
-                select.append("<option>" + key + "</option>")
+            options = cols[i].attr("data-options").split("|");
+            for (var j = 0; j < options.length; j++) {
+                select.append("<option>" + options[j] + "</option>")
             }
-            col.on("change", "select", textFilter);
+            col.on("change", "select", {method: "exact", index: i}, textFilter);
         }
         filterRow.append(col);
     }
     if (needFilterRow) {
-        thead.append(filterRow);
+        $("table#magictable thead").append(filterRow);
     }
 }
 
-function textFilter () {
-    regex = new RegExp("\\b" + $(this).val(), 'i');
-    item = $(this).parent();
-    index = item.parent().children().index(item);
-    rows = $("tbody tr", item.parent().parent().parent());
-    for (var i = 0; i < rows.length; i++) {
-        square = $($(rows[i]).children()[index]);
-        if (square.text().match(regex)) {
+function textFilter (event) {
+    val = $(this).val().replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"); // escaped
+    if (val == "") {
+        val = undefined;
+    }
+    regexstart = new RegExp("\\b" + val, 'i');
+    index = event.data.index;
+    method = event.data.method;
+    for (var y = 0; y < window.tbody.length; y++) {
+        square = window.tbody[y][index];
+        if (method == "exact") {
+            valid = (square.text() == val) || (val == undefined);
+        } else if (method == "startword") {
+            valid = (val == undefined) || square.text().match(regexstart);
+        }
+        row = $(square.parent());
+        if (valid) {
             square.removeClass("nomatch");
+            for (var x = 0; x < window.tbody[y].length; x++) {
+                if (window.tbody[y][x].hasClass("nomatch")) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                row.slideDown();
+            }
         } else {
             square.addClass("nomatch");
-        }
-    };
-    changeDisplay(rows);
-}
-
-function changeDisplay (rows) {
-    for (var i = 0; i < rows.length; i++) {
-        row = $(rows[i]);
-        remove = $("td.nomatch", row).length > 0;
-        removed = row.hasClass("nomatchrow");
-        if (remove && !removed) {
-            row.addClass("nomatchrow");
             row.slideUp();
-        } else if (removed && !remove) {
-            row.removeClass("nomatchrow");
-            row.slideDown();
         }
     }
 }
