44a7554cfa6a70fe954ec34e9d54382d8b554722
matts1 <mattstark75@gmail.com>
2013-08-17 15:10:04 +1000

    Added all the png images... don't want to have to do this, but the shellscript to make them (also in the commit) will only work on linux (and possibly mac)

---
+++ robo_rally/static/images/cards/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir cards
+for i in $(seq 10 10 840); do
+    text=$(printf %03d $i)
+    base="base"
+    if (($i < 70)); then
+        base="uturn"
+    elif ((($i < 430) && ($i % 20 == 10))); then
+        base="rotleft"
+    elif ((i < 430)); then
+        base="rotright"
+    elif ((i < 540)); then
+        base="backup"
+    elif ((i < 670)); then
+        base="move1"
+    elif ((i < 790)); then
+        base="move2"
+    elif ((i < 850)); then
+        base="move3"
+    fi
+    convert -pointsize 15 -fill yellow -draw "text 51,23 \"$text\"" $base.png $i.png
+done
